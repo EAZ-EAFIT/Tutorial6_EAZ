8f4cacdca070781e21662fdf79d02f462a9b4f76
Esteban-Alvarez <84643021+Esteban-Alvarez@users.noreply.github.com>
2025-10-19 16:07:38 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const app = express()
+
+const port = 80
+
+const phrases = [
+    "Get ready to be inspired…",
+    "See rejection as redirection.",
+    "There is beauty in simplicity.",
+    "You can't be late until you show up.",
+    "Maybe life is testing you. Don’t give up.",
+    "Impossible is just an opinion.",
+    "Alone or not you gonna walk forward.",
+    "Sometimes I feel like throwing in the towel, but then with what do I dry myself?",
+    "Sometimes the people who speak the least, are the most silent.",
+    "Looking at a window is like looking at wall but instead of a wall it´s a window.",
+    "A veces me siento solo, y aveces me ayudan a sentarme.",
+    "Having a million dollars is like having a dollar a million times.",
+]
+
+app.get('/', (req, res) => {
+
+    const length = phrases.length;
+    const number = Math.floor(Math.random() * length);
+    res.send(phrases[number])
+
+})
+
+app.listen(port, () => {
+
+    console.log(`Example app listening on port ${port}`)
+
+})
